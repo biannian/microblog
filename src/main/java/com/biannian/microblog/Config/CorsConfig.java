58fb2e2847564a6008080829003917db2ba30feb
bianniana <1124784701@qq.com>
2022-01-20 16:03:32 +0800

    7

--- src/main/java/com/biannian/microblog/Config/CorsConfig.java
+++ src/main/java/com/biannian/microblog/Config/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         // // addAllowedOrigin 不能设置为* 因为与 allowCredential 冲突,需要设置为具体前端开发地址
         corsConfiguration.addAllowedOrigin("http://localhost:8080");//前端的开发地址
-        corsConfiguration.addAllowedOrigin("http://localhost:8081");//前端的开发地址
+        corsConfiguration.addAllowedOrigin("http://121.5.222.148:8080");//前端的开发地址
         corsConfiguration.addAllowedMethod("*");
         // allowCredential 需设置为true
         corsConfiguration.setAllowCredentials(true);
--- src/main/java/com/biannian/microblog/Entity/User.java
+++ src/main/java/com/biannian/microblog/Entity/User.java
@@ -23,4 +23,6 @@ public class User {
      */
     private String likeType;
 
+    private String account;
+
 }
